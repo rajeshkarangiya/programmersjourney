1368d88ccaa10095d879e45a82728550bb9dd92e
Rajesh <111978407+rajeshkarangiya@users.noreply.github.com>
2022-12-19 14:50:27 +0530

    GAME
    PYTHON

--- python/SNAKE WATER AND GUN GAME.py
+++ python/SNAKE WATER AND GUN GAME.py
@@ -11,14 +11,12 @@ a = [1, 2, 3]
 me = 0
 com = 0
 i = 1
-while(i<=10):
+while i <= 10:
     b = random.choice(a)
     x = int(input("Your Turn : "))
     print("Computers Turn : ",b)
-    if(x>3):
-        print("Wrong input by you!!")
-    if x == b:
-        print("Tie!!!!")
+    if(x>3) : print("Wrong input by you!!")
+    if x == b: print("Tie!!!!")
     elif x == 1:
         if b == 2:
             print("Score !!")
@@ -43,9 +41,9 @@ while(i<=10):
     i += 1
 
 print("Game Over "
-      "your score ", me," , "
+      "your score ", me, " , "
       "Computer's score = ", com)
-if(me > com):
+if me > com:
     print("CONGRATULATION!! YOU WIN THE GAME" + emoji.emojize(":party_popper:")*3)
 else:
     print("YOU LOOSE!!!" + emoji.emojize(":pleading_face:")*3)
